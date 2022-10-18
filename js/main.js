f094375624ee645d8c87b5053d8514fce3c4452b
alessandrolega <alessandrolega24@gmail.com>
2022-10-18 15:49:20 +0200

    aggiunto prompt Name + Surname

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+aggiungiamo alla fine un numero
+Infine scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21" */
+
+let Name = prompt('Write Your Name Here:');
+
+console.log(Name)
+
+let Surname = prompt('Write Your Surname Here:');
+
+console.log(Surname)
